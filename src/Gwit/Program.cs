6bc82fd88df9fa8930b6836c0f75c585fc0f43e3
Robert Wilczynski <robert@robertwilczynski.com>
2010-06-21 17:47:55 +0200

    basic gwit launcher

---
+++ src/Gwit/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Gwit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var processInfo = new ProcessStartInfo(@"C:\Program Files (x86)\Common Files\microsoft shared\DevServer\10.0\WebDev.WebServer40.EXE")
+            { 
+                Arguments = @"/port:3361 /nodirlist /path:""c:\dev\Projects\My\gwit\src\Gwit.Web"" /vpath:""/""",
+            };
+            Process.Start(processInfo);
+
+
+
+            var browserProcessInfo = new ProcessStartInfo(@"http://localhost:3361/repo?location=" + Environment.CurrentDirectory);
+            Process.Start(browserProcessInfo);
+
+        }
+    }
+}
